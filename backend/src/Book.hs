25f0fbc51d1680af953160b3713b54dea398e087
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-10-17 17:56:42 +0200

    Add book storage code

--- backend/src/Book.hs
+++ backend/src/Book.hs
@@ -1,19 +1,31 @@
+{-# LANGUAGE DeriveGeneric #-}
+
 module Book
   ( Book(..)
-  , Isbn()
+  , Isbn(..)
   , IsbnParseError()
   , isbnFromString
+  , toIsbn13
+  , jsonToBook
+  , bookToJson
+  , example
   ) where
 
 import           Control.Applicative
 import           Control.Monad
 import           Control.Monad.Trans.Except
+import qualified Data.Aeson                 as A
+import qualified Data.ByteString.Lazy       as BSL
 import           Data.Char
 import           Data.Maybe
+import qualified Data.Text                  as T
+import qualified Data.Text.Encoding         as T
+import           GHC.Generics
 
 data Isbn
   = Isbn10 [Int]
   | Isbn13 [Int]
+  deriving (Generic)
 
 instance Show Isbn where
   show (Isbn10 numbers) = map isbnIntToDigit numbers
@@ -61,6 +73,26 @@ _isbn13FromString isbn = do
 isbnFromString :: String -> Either [IsbnParseError] Isbn
 isbnFromString n = runExcept (_isbn13FromString n <|> _isbn10FromString n)
 
+_isbn13CheckDigit :: [Int] -> Int
+_isbn13CheckDigit = (`mod` 10) . (\a -> -a) . sum . zipWith (*) (cycle [1, 3])
+
+toIsbn13 :: Isbn -> Isbn
+toIsbn13 (Isbn10 numbers) = let
+  isbn13Numbers = [9, 7, 8] ++ init numbers
+  finalDigit = _isbn13CheckDigit isbn13Numbers
+  in Isbn13 $ isbn13Numbers ++ [finalDigit]
+toIsbn13 isbn             = isbn
+
+example :: Book
+example = Book
+ { _title = "This is a book"
+ , _author = ["Author", "are"]
+ , _isbn = Isbn10 [0,3,0,6,4,0,6,1,5,2]
+ , _summary = "This is a book"
+ , _language = "De"
+ , _pages = 20
+ }
+
 data Book = Book
   { _title    :: String
   , _isbn     :: Isbn
@@ -68,4 +100,19 @@ data Book = Book
   , _summary  :: String
   , _pages    :: Int
   , _language :: String
-  }
+  } deriving (Generic, Show)
+
+instance A.ToJSON Isbn where
+  toEncoding = A.genericToEncoding A.defaultOptions
+
+instance A.ToJSON Book where
+  toEncoding = A.genericToEncoding A.defaultOptions
+
+instance A.FromJSON Isbn
+instance A.FromJSON Book
+
+bookToJson :: Book -> T.Text
+bookToJson b = T.decodeUtf8 $ BSL.toStrict $ A.encode b
+
+jsonToBook :: T.Text -> Either String Book
+jsonToBook text = A.eitherDecode (BSL.fromStrict $ T.encodeUtf8 text)
--- backend/src/Storage.hs
+++ backend/src/Storage.hs
@@ -1,7 +1,23 @@
-module Storage.Stoprage where
+module Storage
+  ( loadBook
+  , storeBook
+  ) where
 
 import           Book
-import qualified Data.Text as T
+import qualified Data.Text       as T
 
-bookToJson :: Book -> T.Text
-bookToJson = undefined
+loadBook :: FilePath -> Isbn -> IO (Either String Book)
+loadBook directory isbn = do
+  fileContent <- readFile filePath
+  return $ jsonToBook (T.pack fileContent)
+  where
+    fileName = show (toIsbn13 isbn) ++ ".json"
+    filePath = directory ++ "/" ++ fileName
+
+storeBook :: FilePath -> Book -> IO ()
+storeBook directory book = do
+  writeFile filePath $ T.unpack $ bookToJson book
+  return ()
+  where
+    fileName = show (toIsbn13 (_isbn book)) ++ ".json"
+    filePath = directory ++ "/" ++ fileName
