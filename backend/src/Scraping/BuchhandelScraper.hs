8184d6ccfa9ec2b93e4dccd271ada4441b07b7d8
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2020-10-22 19:17:35 +0200

    [backend] Fix 404 handling in BuchhandelScraper

--- backend/src/Scraping/BuchhandelScraper.hs
+++ backend/src/Scraping/BuchhandelScraper.hs
@@ -8,10 +8,8 @@ module Scraping.BuchhandelScraper
 
 import           Book
 import           Control.Applicative
-import           Control.Monad             (guard)
 import qualified Data.Aeson                as A
 import qualified Data.Aeson.Types          as A
-import qualified Data.ByteString.Char8     as BSC
 import qualified Data.ByteString.Lazy      as BSL
 import qualified Data.List.NonEmpty        as NE
 import qualified Data.Text                 as T
@@ -51,16 +49,15 @@ scrapeBuchhandel isbn = do
   let request = addRequestHeader "Accept" "*/*" $ addRequestHeader "User-Agent" "Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0" initRequest
   response <- httpBS request
 
-  guard $ getResponseStatusCode response == 200
+  if getResponseStatusCode response /= 200 then
+    return Nothing
+  else do
+    let body = BSL.fromStrict $ getResponseBody response
 
-  let body = BSL.fromStrict $ getResponseBody response
-
-  writeFile "/tmp/test/output.json" (BSC.unpack $ BSL.toStrict body)
-
-  case A.decode body of
-    Nothing -> return Nothing
-    Just val  -> do
-      return $ A.parseMaybe startParser val
+    case A.decode body of
+      Nothing -> return Nothing
+      Just val  -> do
+        return $ A.parseMaybe startParser val
 
   where
     lookupUrl = "https://www.buchhandel.de/jsonapi/productDetails/" ++ show isbn
